2846064041a2c97de12563ed7d65dcf24b82b3c5
masato <masato@m-mbp.local>
2018-11-10 14:28:35 +0800

    Initial Commit

---
+++ map_present_iOS_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map_present_iOS_test
+//
+//  Created by masato on 10/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
